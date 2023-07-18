a1421263762aa99b4bc0d485f8078dfce9e19d08
snowcharm <snowcharmqq@gmail.com>
2023-07-18 22:51:17 +0800

    :rotating_light: fix unique_ptr warning

--- a5/triangle.hpp
+++ a5/triangle.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstring>
+#include <memory>
 #include "object.hpp"
 
 bool rayTriangleIntersect(const Vector3f &v0, const Vector3f &v1, const Vector3f &v2, const Vector3f &orig,
