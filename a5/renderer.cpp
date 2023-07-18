9f79bdc17cb9c67a5763f11c6924c03a86ebc408
snowcharm <snowcharmqq@gmail.com>
2023-07-18 22:43:07 +0800

    :sparkles: implement a5 tasks

--- a5/renderer.cpp
+++ a5/renderer.cpp
@@ -236,10 +236,13 @@ void Renderer::Render(const Scene &scene)
             // generate primary ray direction
             float x;
             float y;
-            // TODO: Find the x and y positions of the current pixel to get the direction
+            // Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
             // x (horizontal) variable with the *imageAspectRatio*
+
+            x = (2 * (i + 0.5) / (float)scene.width - 1) * scale * imageAspectRatio;
+            y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
 
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
--- a5/triangle.hpp
+++ a5/triangle.hpp
@@ -6,11 +6,30 @@
 bool rayTriangleIntersect(const Vector3f &v0, const Vector3f &v1, const Vector3f &v2, const Vector3f &orig,
                           const Vector3f &dir, float &tnear, float &u, float &v)
 {
-    // TODO: Implement this function that tests whether the triangle
+    // Implement this function that tests whether the triangle
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
-    return false;
+    auto e1 = v1 - v0;
+    auto e2 = v2 - v0;
+    auto s = orig - v0;
+    auto s1 = crossProduct(dir, e2);
+    auto s2 = crossProduct(s, e1);
+    auto s1e1 = dotProduct(s1, e1);
+    auto t = dotProduct(s2, e2) / s1e1;
+    auto b1 = dotProduct(s1, s) / s1e1;
+    auto b2 = dotProduct(s2, dir) / s1e1;
+    if (t >= 0 && b1 >= 0 && b2 >= 0 && (1 - b1 - b2) >= 0)
+    {
+        tnear = t;
+        u = b1;
+        v = b2;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 class MeshTriangle : public Object
